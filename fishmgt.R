d5766c5a9c82af89bac8e802e9bfbe906f2598eb
julianaherrera <julihc10@gmail.com>
2016-01-15 11:19:20 -0800

    sending you data!

---
+++ fishmgt.R
@@ -0,0 +1,7 @@
+## Fisheries Management group
+## Juliana Herrera
+## Week 2
+`fish number` <- read.csv("~/Documents/UCSB/Winter 2016/Informatics/fishmgt.github.io/fish.csv")
+View(`fish number`)
+plot(`fish number`,col="blue")
+
