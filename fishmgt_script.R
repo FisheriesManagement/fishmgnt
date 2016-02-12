ac13b23614887fc68662bf7894f4df110f1dbac1
julianaherrera <julihc10@gmail.com>
2016-02-12 11:03:39 -0800

    deleted

--- fishmgt_script.R
+++
@@ -1,9 +0,0 @@
-###########################################
-# Fisheries Management Group
-# ESM 296 - Informatics
-# Week 2
-###########################################
-
-Alopias.Data <- read.csv("C:/Users/Caio_Otto/Desktop/Bren School/Winter 16/Informatics/Alopias Data.csv")
-
-View(Alopias.Data)
